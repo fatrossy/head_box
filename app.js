7f2832f7589226c92abf3d54876b65984d422188
fatrossy <doublecheat@gmail.com>
2017-06-20 19:13:51 +0200

    with box content

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 
+var app = document.getElementById('app')
+
+
+app.innerHTML =   '<div class="heading">'+
+                    '<div class="box-group">'+
+                      '<div class="floating-box">'+ '</div>'+
+                      '<div class="floating-box">'+'</div>'+
+                      '<div class="floating-box">'+'</div>'+
+                      '<div class="floating-box">'+'</div>'+
+                      '<div class="floating-box">'+'</div>'+
+                    '</div>'+
+                  '</div>';
+console.log(app);
